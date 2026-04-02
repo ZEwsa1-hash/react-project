0ad1c186536b5595b53494c94265b695f3178e1b
Lin <kolerot51@gmail.com>
2026-04-02 23:14:14 +0300

    refactor(main): remove unused ThemeProvider wrappers from app entry point

--- project/frontend/src/main.tsx
+++ project/frontend/src/main.tsx
@@ -1,19 +1,12 @@
-// src/main.tsx (главный)
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from './App.tsx';
 import { AuthProvider } from './contexts/auth-context';
-import { ThemeProvider } from './pages/todolist/context/ThemeContext';
-import { ThemeContextComponent } from './pages/notice/contexts/ThemeContext'; // 👈 Если нужен
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <AuthProvider>
-      <ThemeProvider>
-        <ThemeContextComponent>
-          <App />
-        </ThemeContextComponent>
-      </ThemeProvider>
+      <App />
     </AuthProvider>
   </BrowserRouter>,
 );
